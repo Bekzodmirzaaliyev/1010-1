a297ab77671e04cacafde324edc25bc4335f0630
Bekzodmirzaaliyev <bekzodmirzaaliyev27@gmail.com>
2024-10-14 10:46:20 +0500

    japan lox

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <header>
+        <Navbar />
+      </header>
+      <main className="flex">
+        <aside>
+          <Sidebar />
+        </aside>
+
+        <section>
+          <Outlet />
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default App;
